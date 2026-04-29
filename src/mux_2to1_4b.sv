f2624d9047be87459299864f6554bbb08272d5a2
Pablo Mendoza Ponce <77683977+pmendozap@users.noreply.github.com>
2026-04-28 21:31:56 -0600

    Update mux_2to1_4b.sv

--- src/mux_2to1_4b.sv
+++ src/mux_2to1_4b.sv
@@ -3,14 +3,15 @@
 * Prof. Dr.-ing. Pablo Mendoza Ponce
 * Rev. 1 28 July 2024
 */
+`timescale 1ns/1ps
+
 module mux_2to1_4b(
   input logic [3:0]  a_i,
   input logic [3:0]  b_i,
   input logic        s_i,
   output logic [3:0] q_o);
 
-  timeunit 1ns;
-  timeprecision 1ps;
+
   
   always_comb begin
     if(s_i == 1'b0) begin

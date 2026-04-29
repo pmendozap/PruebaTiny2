ebd6ccc2608a30fe04cd02de25435a171a2bc528
Pablo Mendoza Ponce <77683977+pmendozap@users.noreply.github.com>
2026-04-28 21:01:27 -0600

    Nuevo project

--- src/project.v
+++ src/project.v
@@ -1,3 +1,5 @@
+
+
 /*
  * Copyright (c) 2024 Your Name
  * SPDX-License-Identifier: Apache-2.0
@@ -17,11 +19,20 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  assign uo_out[7:4]  = 0;
   assign uio_out = 0;
-  assign uio_oe  = 0;
+  assign uio_oe[7:1]  = 0;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena, clk, rst_n, uio_in[7:1], 1'b0};
+
+  assign uio_oe[0] = 1'b0;
+
+  mux_2to1_4b U0 (
+  .a_i(ui_in[3:0]),
+  .b_i(ui_in[7:4]),
+  .s_i(uio_in[0]),
+  .q_o(uo_out[3:0])
+  );
 
 endmodule
